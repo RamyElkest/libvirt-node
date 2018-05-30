ed07740ba089092b49889ddc4773e52a38359aea
ramyelkest <4thkest@gmail.com>
2018-05-30 00:14:38 -0700

    migrating headers to emitters

--- src/generators/headers/default.js
+++
@@ -1,7 +0,0 @@
-let { headers_emitter } = require('../../emitters');
-
-headers_emitter.registerDefault({
-  module: 'header_default',
-  callback: (func) =>
-	`napi_value libvirt_${func.name}(napi_env env, napi_callback_info info);\n`
-});
