9456f6a18718ea89111b3d10184dcb13b204c127
ramyelkest <4thkest@gmail.com>
2018-06-06 05:38:56 +0100

    Addressing comments

--- binding.gyp
+++ binding.gyp
@@ -14,14 +14,6 @@
           'cflags': [
             '<!@(pkg-config --cflags libvirt)'
           ],
-        }],
-        ['OS=="mac"', {
-          'xcode_settings': {
-            'GCC_ENABLE_CPP_RTTI': 'YES',
-            'OTHER_CPLUSPLUSFLAGS' : [ '-std=c++11', '-stdlib=libc++' ],
-            'OTHER_LDFLAGS': [ '-stdlib=libc++' ],
-            'MACOSX_DEPLOYMENT_TARGET': "10.7"
-          }
         }]
       ],
    }
--- test/libvirt.test.js
+++ test/libvirt.test.js
@@ -4,30 +4,33 @@ let libvirt = require('../lib');
 let assert = require('assert');
 
 // virConnect
-function test_virConnectOpen(name) {
-  let conn = libvirt.open(name);
+function test_virConnectOpen(...args) {
+  let conn = libvirt.open(...args);
 }
 
-assert.doesNotThrow(() => test_virConnectOpen(), 'with no uri');
-assert.doesNotThrow(() => test_virConnectOpen('qemu:///system'), 'with default uri');
-assert.throws(() => test_virConnectOpen('malformed'), 'with malformed uri');
-
+assert.doesNotThrow(() => test_virConnectOpen(), 'connect with no uri');
+assert.doesNotThrow(() => test_virConnectOpen('test:///default'), 'connect with default uri');
+assert.doesNotThrow(() => test_virConnectOpen('test:///default', 2, 'Three', 4), 'connect with too many arguments');
+assert.throws(() => test_virConnectOpen('malformed'), 'connect with malformed uri');
+assert.throws(() => test_virConnectOpen(7), 'connect with wrong argument type');
 
 // virDomain
-function test_virDomainLookupByName(name) {
-  let conn = libvirt.open(),
-      dom  = conn.lookupByName(name);
+function test_virDomainLookupByName(...args) {
+  let conn = libvirt.open('test:///default'),
+      dom  = conn.lookupByName(...args);
 }
 function test_virDomainGetId() {
-  let conn = libvirt.open(),
-      dom  = conn.lookupByName('firsttest'),
+  let conn = libvirt.open('test:///default'),
+      dom  = conn.lookupByName('test'),
       id   = dom.getId();
   console.log("domain id is", id);
   return id;
 }
 
-assert.doesNotThrow(() => test_virDomainLookupByName('firsttest'), 'with correct name');
-assert.throws(() => test_virDomainLookupByName('lasttest'), 'with incorrect name');
-assert.strictEqual(test_virDomainGetId(), 3);
+assert.doesNotThrow(() => test_virDomainLookupByName('test'), 'domain lookup with correct name');
+assert.doesNotThrow(() => test_virDomainLookupByName('test', 2, 'Three', 4), 'domain lookup with too many arguments');
+assert.throws(() => test_virDomainLookupByName('lasttest'), 'domain lookup with incorrect name');
+assert.throws(() => test_virDomainLookupByName(false), 'domain lookup with wrong argument type');
+assert.strictEqual(test_virDomainGetId(), 1);
 
 console.log('All tests passed!');
